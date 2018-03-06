5ba321bf7f13ce5b201936387deab760c7819e71
Aric <arickuter99@gmail.com>
2018-03-06 20:28:01 +0200

    Enemy movement Collision detection Win detection Reset when collision with enemy

--- js/app.js
+++ js/app.js
@@ -8,12 +8,58 @@ var Enemy = function() {
   this.sprite = 'images/enemy-bug.png';
 };
 
+var allEnemies = [];
+for (i = 0; i < 6; i++) {
+  allEnemies.push(new Enemy());
+  allEnemies[i].x = -100;
+  allEnemies[i].width = 70;
+  allEnemies[i].height = 65;
+  allEnemies[i].speed = ((Math.random() * 300) + 100);
+  if (allEnemies.length < 3) {
+    allEnemies[i].y = 60;
+  } else if (allEnemies.length > 2 && allEnemies.length < 5) {
+    allEnemies[i].y = 145;
+  } else {
+    allEnemies[i].y = 230;
+  }
+}
+
+function checkCollision() {
+  for (i = 0; i < 6; i++) {
+    if (player.x < allEnemies[i].x + allEnemies[i].width && player.x + player.width > allEnemies[i].x &&
+      player.y < allEnemies[i].y + allEnemies[i].height && player.y + player.height > allEnemies[i].y) {
+      player.x = 200;
+      player.y = 300;
+      alert('You died!');
+      break;
+    }
+  }
+}
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
+
+  checkCollision();
+
+  for (i = 0; i < 6; i++) {
+    if (allEnemies[i].x > 500) {
+      allEnemies[i].x = -100;
+    }
+  }
+
+  var enemyNum = Math.floor(Math.random() * 6);
+  var enemyObj = allEnemies[enemyNum];
+  if (enemyObj.x > -100) {
+    enemyObj.x += enemyObj.speed * dt;
+  } else {
+    setTimeout(function() {
+      enemyObj.x += 1;
+    }, Math.random() * 100000);
+  }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,14 +71,21 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-
   this.sprite = 'images/char-boy.png';
   this.x = 200;
   this.y = 300;
+  this.width = 65;
+  this.height = 76;
 };
 
 Player.prototype.update = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  if (this.y === -20) {
+    setTimeout(function() {
+      alert('You win!');
+      location.reload();
+    }, 50);
+  }
 };
 
 Player.prototype.render = function() {
@@ -40,16 +93,16 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.handleInput = function(direction) {
-  if (direction === 'up') {
+  if (direction === 'up' && this.y !== -20) {
     this.y -= 80;
 
-  } else if (direction === 'down') {
+  } else if (direction === 'down' && this.y !== 380) {
     this.y += 80;
 
-  } else if (direction === 'left') {
+  } else if (direction === 'left' && this.x !== 0) {
     this.x -= 100;
 
-  } else if (direction === 'right') {
+  } else if (direction === 'right' && this.x !== 400) {
     this.x += 100;
 
   }
@@ -58,19 +111,21 @@ Player.prototype.handleInput = function(direction) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
+
 var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-  var allowedKeys = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-  };
+setTimeout(function() {
+  document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
+    };
 
-  player.handleInput(allowedKeys[e.keyCode]);
-});
+    player.handleInput(allowedKeys[e.keyCode]);
+  });
+}, 2000);
